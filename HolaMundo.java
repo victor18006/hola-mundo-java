345031a78ad71b6e33005e4de42f34eae6a74c06
VictorO <vosorio@uabc.edu.mx>
2025-02-12 10:05:07 -0800

    Modificación para desplegar mi nombre

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,4 +2,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo desde Java!");
     }
+public nuevaLinea() {
+        System.out.println("Nueva Linea!");
+    }
 }
